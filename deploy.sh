fd0afaa3c4b3c9811158505d81321439db397e9c
Aaron Chan <aarchan108@gmail.com>
2026-05-16 22:11:30 -0400

    add build command

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,23 @@ PI_USER="${PI_USER:-aaron}"
 PI_HOST="${PI_HOST:-atlas.local}"
 PI_WORKSPACE="${PI_WORKSPACE:-/workspace}"
 
+build_workspace() {
+    if [ -f "/.dockerenv" ] || [ -f "/opt/ros/humble/setup.bash" ]; then
+        colcon build \
+            --symlink-install \
+            --event-handlers console_cohesion+ \
+            --cmake-args -G Ninja
+    else
+        LOCAL_UID=$(id -u) LOCAL_GID=$(id -g) docker compose run --rm ros bash -lc \
+            "cd /workspace && \
+            source /opt/ros/humble/setup.bash && \
+            if [ -f /workspace/install/setup.bash ]; then source /workspace/install/setup.bash; fi && \
+            colcon build --symlink-install --event-handlers console_cohesion+ --cmake-args -G Ninja"
+    fi
+}
+
+build_workspace
+
 echo "Syncing install/ to ${PI_USER}@${PI_HOST}:${PI_WORKSPACE}/install/"
 
 rsync -avz --delete \
